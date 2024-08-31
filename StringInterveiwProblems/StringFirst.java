f2ee1d73c6ab95ba12dae152a9f252679c1aa2f2
vishal rai <vishalrai10797@gmail.com>
2024-09-01 00:01:09 +0530

    islomorphic

---
+++ StringInterveiwProblems/StringFirst.java
@@ -0,0 +1,13 @@
+package StringInterveiwProblems;
+
+public class StringFirst {
+  public static void main(String[] args) {
+    String str = " vishal rai ";
+
+    for (int i = 0; i < str.length(); i++) {
+
+      System.out.print(str);
+    }
+  }
+
+}
