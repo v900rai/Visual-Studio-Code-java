6f9e8993203e9037719a5ed4df9623fcdc88fa2d
vishal rai <vishalrai10797@gmail.com>
2024-08-31 11:30:08 +0530

    unknown

---
+++ Print.java
@@ -0,0 +1,15 @@
+/**
+ * Print-0-5-10
+ */
+public class Print {
+
+  public static void main(String args[]) {
+    for (int i = 0; i <= 100; i = i + 5) {
+      System.out.print(i + " , ");
+
+    }
+    System.out.println();
+
+  }
+
+}
---
+++ mathsProgram/AddTwoNumber.java
@@ -0,0 +1,12 @@
+package mathsProgram;
+
+public class AddTwoNumber {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c;
+    c = a + b;
+    System.out.println(c);
+  }
+
+}
