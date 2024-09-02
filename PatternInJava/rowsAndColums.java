273cef8f590c421469a281ec480d84e2bea2710a
vishal rai <vishalrai10797@gmail.com>
2024-09-02 13:30:08 +0530

    rowsandcolums

---
+++ PatternInJava/rowsAndColums.java
@@ -0,0 +1,14 @@
+package PatternInJava;
+
+public class rowsAndColums {
+  // how to print rows and colums print form
+  public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+      for (int j = 1; j <= 4; j++) {
+        System.out.print("*");
+      }
+      System.out.println();
+    }
+  }
+
+}
