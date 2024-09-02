a167e27f5aa70404d312051571ed3eecc3b493c0
vishal rai <vishalrai10797@gmail.com>
2024-09-02 15:52:07 +0530

    Ternary

---
+++ TypesOfOperatorsInJava/LogicalOperators.java
@@ -0,0 +1,14 @@
+package TypesOfOperatorsInJava;
+
+public class LogicalOperators {
+  public static void main(String[] args) {
+    // Logical operators
+    boolean x = true;
+    boolean y = false;
+
+    System.out.println("x && y: " + (x && y));
+    System.out.println("x || y: " + (x || y));
+    System.out.println("!x: " + (!x));
+  }
+
+}
---
+++ TypesOfOperatorsInJava/Ternaryoperator.java
@@ -0,0 +1,14 @@
+package TypesOfOperatorsInJava;
+
+public class Ternaryoperator {
+  public static void main(String[] args) {
+    int a = 20, b = 10, c = 30, result;
+
+    // result holds max of three
+    // numbers
+    result = ((a > b) ? (a > c) ? a : c : (b > c) ? b : c);
+    System.out.println("Max of three numbers = "
+        + result);
+  }
+
+}
