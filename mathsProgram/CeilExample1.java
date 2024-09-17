408efb1af11ffab964784fbeb505c9ebe4920feb
vishal rai <vishalrai10797@gmail.com>
2024-09-17 14:59:06 +0530

    ceil value

---
+++ mathsProgram/CeilExample1.java
@@ -0,0 +1,11 @@
+package mathsProgram;
+
+public class CeilExample1 {
+
+  public static void main(String[] args) {
+    double x = 83.56;
+    // Input positive value, Output ceil value of x
+    System.out.println(Math.ceil(x));
+
+  }
+}
---
+++ mathsProgram/RoundExample1.java
@@ -0,0 +1,10 @@
+package mathsProgram;
+
+public class RoundExample1 {
+  public static void main(String[] args) {
+    double x = 79.49;
+    // find the closest int for the double
+    System.out.println(Math.round(x));
+
+  }
+}
