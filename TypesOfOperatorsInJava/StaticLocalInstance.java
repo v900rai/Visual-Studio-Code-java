5dff40d9df273a0f25b1193788d22f8de8f9f07f
vishal rai <vishalrai10797@gmail.com>
2024-09-17 15:54:01 +0530

    localStatic instnace variable

---
+++ TypesOfOperatorsInJava/StaticLocalInstance.java
@@ -0,0 +1,15 @@
+package TypesOfOperatorsInJava;
+
+public class StaticLocalInstance {
+
+  static int m = 100;// static variable
+
+  void method() {
+    int n = 90;// local variable
+  }
+
+  public static void main(String args[]) {
+    int data = 50;// instance variable
+  }
+
+}
