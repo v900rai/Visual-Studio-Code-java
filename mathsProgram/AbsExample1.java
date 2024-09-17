5c0b7917cda5140ac01575a6bf484cff80f9ab4e
vishal rai <vishalrai10797@gmail.com>
2024-09-17 14:25:58 +0530

    abs

---
+++ mathsProgram/AbsExample1.java
@@ -0,0 +1,18 @@
+package mathsProgram;
+
+/**
+ * AbsExample1
+ */
+public class AbsExample1 {
+  public static void main(String[] args) {
+
+    int x = 78;
+    int y = -48;
+    // print the absolute value of int type
+    System.out.println(Math.abs(x));
+    System.out.println(Math.abs(y));
+    System.out.println(Math.abs(Integer.MIN_VALUE));
+
+  }
+
+}
