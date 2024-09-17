ee2c37b759abf5be3b0a28615db07c9798356d35
vishal rai <vishalrai10797@gmail.com>
2024-09-17 14:48:25 +0530

    second

---
+++ mathsProgram/AbsExample2.java
@@ -0,0 +1,14 @@
+package mathsProgram;
+
+public class AbsExample2 {
+
+  public static void main(String args[]) {
+    double x = -47.63;
+    double y = -894.37;
+    // print the absolute value of double type
+    System.out.println(Math.abs(x));
+    System.out.println(Math.abs(y));
+    System.out.println(Math.abs(7.0 / 0));
+
+  }
+}
